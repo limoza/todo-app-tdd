bde87f6cbdea6926dad662592d95985c43511dbf
limoza <svacc.nelllely@gmail.com>
2023-06-24 22:39:21 +0900

    Headerコンポーネントのテスト作成

--- src/components/Header/Header.test.tsx
+++ src/components/Header/Header.test.tsx
@@ -2,7 +2,18 @@ import { render, screen } from '@testing-library/react'
 import { Header } from './Header'
 
 describe('Header components Test', () => {
-  it.todo('header要素が1つだけ表示されている')
-  it.todo('h1タグが1つだけ表示されている')
-  it.todo('h1タグの内容がTDD ToDo Appである')
+  it('header要素が1つだけ表示されている', () => {
+    render(<Header />)
+    expect(screen.getAllByRole('banner')).toHaveLength(1)
+  })
+  it('h1タグが1つだけ表示されている', () => {
+    render(<Header />)
+    expect(screen.getAllByRole('heading', { level: 1 })).toHaveLength(1)
+  })
+  it('h1タグの内容がTDD ToDo Appである', () => {
+    render(<Header />)
+    expect(
+      screen.getAllByRole('heading', { level: 1, name: 'TDD ToDo App' })
+    ).toBeInTheDocument()
+  })
 })
