94c57790121a3469dcc1d4959b0c5f8f201243d8
limoza <svacc.nelllely@gmail.com>
2023-06-25 22:20:35 +0900

    ButtonのテストのToDoを追加

--- src/components/common/Button/Button.test.tsx
+++ src/components/common/Button/Button.test.tsx
@@ -2,5 +2,7 @@ import { render, screen } from '@testing-library/react'
 import { Button } from './Button'
 
 describe('Button components Test', () => {
-  it.todo('')
+  it.todo('button要素が表示されている')
+  it.todo('button要素のテキストはxxxである')
+  it.todo('button要素をクリックすれば関数が動く')
 })
