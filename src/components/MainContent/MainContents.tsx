70d00edcf22f9e72ae5bbdd0ee49d4372292fc2c
limoza <svacc.nelllely@gmail.com>
2023-06-25 22:08:19 +0900

    基本的な構造を作成する

--- src/components/MainContent/MainContents.tsx
+++ src/components/MainContent/MainContents.tsx
@@ -1,1 +1,39 @@
-export const MainContent = () => <main>mainContent</main>
+export const MainContent = () => {
+  const todos = [
+    {
+      id: 1,
+      text: 'ToDoText',
+      status: 'undone',
+    },
+  ]
+  return (
+    <main>
+      <div>
+        <ul>
+          {todos.map((todo) => {
+            return (
+              <li key={`todo-${todo.id}`}>
+                <input
+                  type="checkbox"
+                  id={`todo-${todo.id}`}
+                  checked={todo.status === 'done'}
+                />
+                <label htmlFor={`todo-${todo.id}`}>{todo.text}</label>
+                <button>編集</button>
+              </li>
+            )
+          })}
+          <li>
+            <label htmlFor="ToDoTextInput">
+              <input type="text" id="ToDoTextInput" />
+              <button>追加</button>
+            </label>
+          </li>
+        </ul>
+        <div>
+          <button>完了したToDoを表示</button>
+        </div>
+      </div>
+    </main>
+  )
+}
